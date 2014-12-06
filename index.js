67f9e3484f3478662c508a47ca759e0868cfcb64
Sergey Chikuyonok <serge.che@gmail.com>
2014-12-07 00:27:07 +0300

    Use pathfinder for CSS rules searching

--- index.js
+++ index.js
@@ -8,19 +8,52 @@
 	if (typeof define === 'function' && define.amd) {
 		// AMD. Register as an anonymous module.
 		define(['exports'], function (exports) {
-			factory(exports);
+			factory(require, exports);
 		});
 	} else if (typeof exports === 'object') {
 		// CommonJS
-		factory(exports);
+		factory(require, exports);
 	} else {
 		// Browser globals
-		factory((root.livestyleCSSOM = {}));
+		factory(require, (root.livestyleCSSOM = {}));
 	}
-}(this, function (exports) {
-	function last(arr) {
-		return arr[arr.length - 1];
+}(this, function(require, exports) {
+	var pathfinder = require('livestyle-pathfinder');
+
+	/**
+	 * Node path shim
+	 */
+	function NodePath(path) {
+		if (Array.isArray(path)) {
+			this.components = path.map(NodePathComponent);
+		} else {
+			this.components = [];
+		}
 	}
+
+	NodePath.prototype.toString = function(skipPos) {
+		return this.components.map(function(c) {
+			return c.valueOf(skipPos);
+		}).join('/');
+	};
+
+	function NodePathComponent(name, pos) {
+		if (!(this instanceof NodePathComponent)) {
+			return new NodePathComponent(name, pos);
+		}
+
+		if (Array.isArray(name)) {
+			pos = name[1];
+			name = name[0];
+		}
+
+		this.name = name;
+		this.pos = pos || 1;
+	}
+
+	NodePathComponent.prototype.toString = function(skipPos) {
+		return this.name +  (!skipPos || this.pos > 1 ? '|' + this.pos : '');
+	};
 
 	/**
 	 * Findes all stylesheets in given context, including
@@ -108,15 +141,12 @@
 	 * @param  {Array} patch
 	 */
 	function patchRule(rule, patch) {
-		if ('value' in patch) {
-			// updating full value of section, like `@import`
-			return patchRuleByValue(rule, patch);
-		}
-
 		if (!rule || !rule.style) {
 			// not a CSSStyleRule, aborting
 			return;
 		}
+
+		// TODO handle @-rules
 
 		// remove properties
 		patch.remove.forEach(function(prop) {
@@ -129,166 +159,55 @@
 		}).join('');
 	}
 
-	function patchRuleByValue(rule, patch) {
-		rule.cssText = ruleName(rule) + ' ' + patch.value;
+	function setupFromPartialMatch(match) {
+		// The `rest` property means we didn’t found exact section
+		// where patch should be applied, but some of its parents.
+		// In this case we have to re-create the `rest` sections
+		// in best matching parent
+		var accumulated = match.rest.reduceRight(function(prev, cur) {
+			return cur.name + ' {' + prev + '}';
+		}, '');
+
+		var parent = match.parent;
+		try {
+			var ix = parent.ref.insertRule(accumulated, match.index);
+		} catch (e) {
+			console.warn('LiveStyle:', e.message);
+			return;
+		}
+
+		var ctx = parent.ref.cssRules[ix];
+		var indexed = exports.createIndex(ctx);
+		indexed.name = ruleName(ctx);
+		indexed.ix = ix;
+		parent.children.splice(ix, 0, indexed);
+		for (var i = ix + 1, il = parent.children.length; i < il; i++) {
+			parent.children.ix++;
+		}
+
+		while (ctx.cssRules && ctx.cssRules.length) {
+			ctx = ctx.cssRules[0];
+		}
+
+		return ctx;
 	}
 
-	/**
-	 * Locates CSS section by given selectors in sections
-	 * list
-	 * @param  {Array} list Plain list of available sections
-	 * @param  {String} sel  Selector to find
-	 * @return {Object}
-	 */
-	function locate(list, sel) {
-		var key = stringifyPath(sel, false);
-		for (var i = 0, il = list.length, item; i < il; i++) {
-			if (list[i].pathString === key) {
-				return list[i];
-			}
-		}
+	function deleteRuleFromMatch(match) {
+		parent(match.node.ref).deleteRule(match.ix);
+		var ix = match.parent.children.indexOf(match);
+		match.parent.children.splice(ix, 1);
 	}
 
-	/**
-	 * Creates string representation of CSS path
-	 * @param  {Array} path
-	 * @return {String}
-	 */
-	function stringifyPath(path) {
-		var out = '';
-		for (var i = 0, il = path.length, p; i < il; i++) {
-			p = path[i];
-			out += (i ? '/' : '') + p[0] + (p[1] > 1 ? '|' + p[1] : '');
-		}
-		return out;
-	}
-
-	function guessLocation(stylesheet, path) {
-		path = path.slice(0);
-		var part, rule, candidates;
-
-		var find = function(collection, path) {
-			if (!collection) {
-				return false;
+	function normalizeHints(hints) {
+		return hints.map(function(hint) {
+			if (hint.before) {
+				hint.before = hint.before.map(NodePathComponent);
 			}
-
-			if (Array.isArray(path)) {
-				path = path[0];
+			if (hint.after) {
+				hint.after = hint.after.map(NodePathComponent);
 			}
-
-			return collection.filter(function(item) {
-				return item.name === path;
-			});
-		};
-
-		var ctx = stylesheet;
-		while (part = path.shift()) {
-			rule = locate(ctx.children, [part]);
-			if (!rule) {
-				candidates = find(ctx.children, part);
-				if (candidates.length) {
-					if (path[0]) {
-						// try to find last node containing
-						// next child
-						rule = last(candidates.filter(function(item) {
-							return find(item.children, path[0]).length;
-						}));
-					}
-
-					if (!rule) {
-						rule = last(candidates);
-					}
-				}
-			}
-
-			if (!rule) { // nothing found, stop here
-				path.unshift(part);
-				break;
-			} else {
-				ctx = rule;
-			}
-		}
-
-		return {
-			found: ctx !== stylesheet,
-			rule: ctx,
-			rest: path.length ? path : null
-		};
-	}
-
-	/**
-	 * Tries to find best partial match of given path on CSS tree and returns
-	 * target section
-	 * @param  {CSSRule} rule CSS tree
-	 * @param  {Array} cssPath Parsed CSS path
-	 */
-	function bestPartialMatch(stylesheet, cssPath, patch) {
-		var loc = guessLocation(stylesheet, cssPath);
-		if (loc.rule.parent) {
-			var ctxName = loc.rule.name;
-			if (ctxName == '@import' && patch.action == 'add') {
-				// in case of added @import rule, make sure it was added,
-				// not replaced existing rule
-				if (!loc.rest) {
-					loc.rest = [];
-				}
-
-				loc.rest.unshift([ctxName]);
-				loc.rule = loc.rule.parent;
-			}
-		}
-
-		ctx = loc.rule.ref;
-
-		if (loc.rest) {
-			// The `rest` property means we didn’t found exact section
-			// where patch should be applied, but some of its parents.
-			// In this case we have to re-create the `rest` sections
-			// in best matching parent
-			var accumulated = '';
-			while (subrule = loc.rest.pop()) {
-				accumulated = subrule[0] + '{' + accumulated + '}';
-			}
-
-			try {
-				var ix = ctx.insertRule(accumulated, ctx.cssRules.length);
-			} catch (e) {
-				console.warn('LiveStyle:', e.message);
-				return;
-			}
-
-			ctx = ctx.cssRules[ix];
-			while (ctx.cssRules && ctx.cssRules.length) {
-				ctx = ctx.cssRules[0];
-			}
-
-			// var subrule, ix;
-			// while (subrule = loc.rest.shift()) {
-			// 	if (!ctx.insertRule) {
-			// 		// can’t insert rule here, so can’t patch the source properly
-			// 		return;
-			// 	}
-			// 	ix = ctx.insertRule(subrule[0] + ' {}', ctx.cssRules.length);
-			// 	ctx = ctx.cssRules[ix];
-			// }
-		}
-
-		patchRule(ctx, patch);
-	}
-
-	function makeList(ctx, out) {
-		out = out || [];
-		var items = ctx.children;
-		if (!items.length) {
-			return out;
-		}
-
-		for (var i = 0, il = items.length; i < il; i++) {
-			out.push(items[i]);
-			makeList(items[i], out);
-		}
-
-		return out;
+			return hint;
+		});
 	}
 
 	/**
@@ -298,18 +217,6 @@
 	 */
 	exports.stylesheets = function() {
 		return findStyleSheets(document.styleSheets);
-	};
-
-	/**
-	 * Returns plain list of all available rules in stylesheet
-	 * @param  {CSSStyleSheet} stylesheet
-	 * @return {Array}
-	 */
-	exports.toList = function(stylesheet, options) {
-		if (!stylesheet.ref) {
-			stylesheet = this.createIndex(stylesheet);
-		}
-		return makeList(stylesheet);
 	};
 
 	/**
@@ -337,64 +244,59 @@
 		}
 
 		patches.forEach(function(patch) {
-			var cssPath = patch.path;
-			var ruleList = self.toList(index);
-			var match = locate(ruleList, cssPath);
-
-			if (match) {
-				if (patch.action === 'remove') {
-					return parent(match.ref).deleteRule(match.ix);
-				}
-				patchRule(match.ref, patch);
-			} else if (patch.action !== 'remove') {
-				bestPartialMatch(index, cssPath, patch);
+			var path = new NodePath(patch.path);
+			var hints = hints ? normalizeHints(patch.hints) : null;
+			var location = pathfinder.find(index, path, hints);
+			if (location.partial && patch.action === 'remove') {
+				// node is absent, do nothing
+				return;
 			}
+
+			if (!location.partial) {
+				// exact match on node
+				if (patch.action === 'remove') {
+					return deleteRuleFromMatch(location);
+				}
+
+				return patchRule(location.node.ref, patch);
+			}
+
+			patchRule(setupFromPartialMatch(location), patch);
 		});
 
 		return stylesheet;
 	};
 
 	exports.createIndex = function(ctx, parent) {
-		var lookup = {};
-		var rule, name, item, path;
+		var indexOf = function(item) {
+			return this.children.indexOf(item);
+		};
 
 		if (!parent) {
 			parent = {
 				ix: -1,
-				name: ':root',
-				path: null,
-				pathString: '',
+				name: '',
 				parent: null,
 				children: [],
-				ref: ctx
+				ref: ctx,
+				indexOf: indexOf
 			};
 		}
 
 		var rules = ctx.cssRules;
 		if (!rules) {
-			return;
+			return parent;
 		}
 
-		for (var i = 0, il = rules.length; i < il; i++) {
+		for (var i = 0, il = rules.length, rule, item; i < il; i++) {
 			rule = rules[i];
-			name = ruleName(rule);
-			if (name in lookup) {
-				lookup[name]++;
-			} else {
-				lookup[name] = 1;
-			}
-
-			path = parent.path ? parent.path.slice(0) : [];
-			path.push([name, lookup[name]]);
-
 			item = {
 				ix: i,
-				name: name,
-				path: path,
-				pathString: stringifyPath(path),
+				name: ruleName(rule),
 				parent: parent,
 				children: [],
-				ref: rule
+				ref: rule,
+				indexOf: indexOf
 			};
 
 			parent.children.push(item);
